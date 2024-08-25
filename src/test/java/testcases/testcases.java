3c68a2c86971a865581afe1289b352377f7ce2dc
amreidkok <eng.amrhassan1013@gmail.com>
2024-08-25 17:43:27 +0300

    test

---
+++ src/test/java/testcases/testcases.java
@@ -0,0 +1,15 @@
+package testcases;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class testcases {
+
+
+    @Test
+    public void testcase1()
+    {
+        System.out.println("amr hassan");
+        Assert.assertEquals(1,1);
+    }
+}
